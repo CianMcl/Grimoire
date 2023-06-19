f77e12da6073ff8b1e7ae9dfc97fc62380d2d282
CianM <cmcl0022@student.monash.edu>
2023-06-19 13:39:26 +1000

    recyclerview bits

--- app/src/main/java/com/example/grimoire/fragment/WbFragment.java
+++ app/src/main/java/com/example/grimoire/fragment/WbFragment.java
@@ -6,12 +6,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.grimoire.R;
 import com.example.grimoire.databinding.NavCharFragmentBinding;
 import com.example.grimoire.databinding.NavCommFragmentBinding;
 import com.example.grimoire.databinding.NavHomeFragmentBinding;
 import com.example.grimoire.databinding.NavProfFragmentBinding;
 import com.example.grimoire.databinding.NavWbFragmentBinding;
+import com.example.grimoire.recycle.RandomNumListAdapter;
 import com.example.grimoire.viewmodel.SharedViewModel;
 
 public class WbFragment extends Fragment {
@@ -21,9 +25,30 @@ public class WbFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-// Inflate the View for this fragment
+
+        //recycler view
+        RecyclerView recyclerView;
+
+        // Inflate the View for this fragment
         addBinding = NavWbFragmentBinding.inflate(inflater, container, false);
         View view = addBinding.getRoot();
+
+        recyclerView = view.findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(new RandomNumListAdapter(1234));
+
+
+
+
+
+
+
+
+
+
+
+
         return view;
     }
     @Override
---
+++ app/src/main/java/com/example/grimoire/recycle/RecyclerViewHolder.java
@@ -0,0 +1,24 @@
+package com.example.grimoire.recycle;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.grimoire.R;
+
+//code from https://medium.com/swlh/create-recyclerview-in-android-fragment-c0f0b151125f
+
+public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView view;
+    public RecyclerViewHolder(@NonNull View itemView) {
+        super(itemView);
+        view = itemView.findViewById(R.id.randomText);
+    }
+
+    public TextView getView(){
+        return view;
+    }
+}
